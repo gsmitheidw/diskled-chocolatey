0609031a7f1d40349f910d735d339d3639451dc5
Graham Smith <gsmitheidw@gmail.com>
2020-11-29 14:15:25 +0000

    initial push

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = 'https://helgeklein.com/downloads/DiskLED/current/DiskLED.zip'
+
+  softwareName  = 'diskled*' 
+
+
+  checksum      = '880C891CF080ECE6FC39E80A492F4231D8715A8F786A54C8E5F9BB59D88C6454'
+  checksumType  = 'sha256' 
+  
+}
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir $url -checksum $checksum -checksumType $checksumType 
