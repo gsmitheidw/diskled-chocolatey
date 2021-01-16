045d8a33b296d51ff41b1c5b2b84af72a9f073c0
Graham Smith <gsmitheidw@gmail.com>
2021-01-16 18:44:37 +0000

    Redundant lines and licence removed

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -7,9 +7,7 @@ $packageArgs = @{
   url           = 'https://helgeklein.com/downloads/DiskLED/current/DiskLED.zip'
   softwareName  = 'diskled*' 
   checksum      = '880C891CF080ECE6FC39E80A492F4231D8715A8F786A54C8E5F9BB59D88C6454'
-  checksumType  = 'sha256' 
-  checksum64	= "$checksum"
-  checksumType64 = 'sha256'
+  checksumType  = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
